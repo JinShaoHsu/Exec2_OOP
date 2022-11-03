7cbb13131031b94109c8d1b1db542a7adc97a74a
ispan <ispan@DESKTOP-PAKSETB>
2022-11-03 10:50:53 +0800

    加入專案檔案。

---
+++ Q1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string value = string.Empty;
+			int[] ans = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			Random random = new Random();
+			ans = ans.OrderBy(x => random.Next()).ToArray();
+			for(int i = 0; i < 4; i++)
+			{
+				value+=(ans[i]).ToString();
+			}
+			answerlabel.Text = value;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			int a = 0;int b = 0;
+			string input = textBox1.Text;
+			string answer = answerlabel.Text;
+
+			int[] aa = new int[] { input[0], input[1],input[2], input[3] };
+			int[] ans = new int[] { answer[0], answer[1], answer[2], answer[3] };
+			for(int i = 0; i < aa.Length; i++)
+			{
+				for(int j = 0; j < ans.Length; j++)
+				{
+					if(ans[i] == aa[j])
+					{
+						b++;
+						if (i == j)
+						{
+							b = b - 1;
+							a = a + 1;
+						}
+					}
+				}
+				ABlabel.Text = $"{a}A{b}B";
+			}
+		}
+	}
+}
---
+++ Q2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q2
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//寫不出來
+			/*亂數取得四個點數
+			 * 如果點數不同就重新,直到至少兩顆相同
+			 * 若兩顆相同,例如2236就傳回9
+			 * 若三顆相同,例如2226就傳回8
+			 * 若四顆相同,例如2222就傳回4
+			*/
+			Random random = new Random();
+		}
+	}
+}
